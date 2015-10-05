3a21458d6ad2d99fb009cd44a878702f8dcab1f3
Karan Sharma <karan1276@gmail.com>
2015-10-05 08:02:25 +0530

    wrote a display function

--- queue/queueAdt.c
+++ queue/queueAdt.c
@@ -11,6 +11,7 @@ void init(queue *ptr){
 	ptr->front=-1;
 	ptr->back=-1;
 }
+
 int full(queue *ptr){
 	if(ptr->back==MAX-1){
 		return 1;
@@ -19,17 +20,32 @@ int full(queue *ptr){
 		return 0;
 	}
 }
+
+
 void insert(queue *ptr, int elem){
 	if(full(ptr)){
 		printf("Queue is full, insertion not performed");
 	}
 	else{
-		back++;
+		ptr->back++;
 		ptr->data[ptr->back]=elem;
 	}
 }
 
+void display(queue *ptr){
+	int i,j;
+	printf("Content of queue is:\n");
+	for(i=ptr->back;i>ptr->front;i--){
+		printf(" %d",ptr->data[i]);
+	}
+}
 int main(){
 	
+	queue q;
+	init(&q);
+	insert(&q,1);
+	insert(&q,2);
+	insert(&q,3);
+	display(&q);
 	return 0;
 }
