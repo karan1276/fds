c5c4c3383b4b7516b6b31f5a7e7e3a648004f155
Karan Sharma <karan1276@gmail.com>
2015-10-05 07:53:02 +0530

    wrote functions full and insert

--- queue/queueAdt.c
+++ queue/queueAdt.c
@@ -6,9 +6,27 @@ typedef struct queue{
 	int data[MAX];
 	int front,back;
 } queue;
+
 void init(queue *ptr){
 	ptr->front=-1;
 	ptr->back=-1;
+}
+int full(queue *ptr){
+	if(ptr->back==MAX-1){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+void insert(queue *ptr, int elem){
+	if(full(ptr)){
+		printf("Queue is full, insertion not performed");
+	}
+	else{
+		back++;
+		ptr->data[ptr->back]=elem;
+	}
 }
 
 int main(){
